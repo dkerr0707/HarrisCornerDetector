0910f0b2db1b56ed496eadfbd61dd75094a24938
dkerr0707 <dkerr0707@gmail.com>
2019-02-05 13:02:27 -0500

    Initial Commit

---
+++ HarrisCornerDetector/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HarrisCornerDetector
+//
+//  Created by David Kerr on 2019-02-05.
+//  Copyright © 2019 David Kerr. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
