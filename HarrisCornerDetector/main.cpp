a084f30ded76914c88aa1f175e4c251ab9a2af0d
dkerr0707 <dkerr0707@gmail.com>
2019-02-05 13:47:59 -0500

    Working image viewer

--- HarrisCornerDetector/main.cpp
+++ HarrisCornerDetector/main.cpp
@@ -6,10 +6,31 @@
 //  Copyright © 2019 David Kerr. All rights reserved.
 //
 
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main( int argc, char** argv )
+{
+    if( argc != 2)
+    {
+        std::cout <<" Usage: display_image ImageToLoadAndDisplay" << std::endl;
+        return -1;
+    }
+
+    
+    cv::Mat image;
+    image = cv::imread(argv[1], CV_LOAD_IMAGE_COLOR);   // Read the file
+    
+    if(! image.data )                              // Check for invalid input
+    {
+        std::cout <<  "Could not open or find the image" << std::endl ;
+        return -1;
+    }
+    
+    namedWindow( "Display window", cv::WINDOW_AUTOSIZE );// Create a window for display.
+    imshow( "Display window", image );                   // Show our image inside it.
+    
+    cv::waitKey(0);                                          // Wait for a keystroke in the window
     return 0;
 }
