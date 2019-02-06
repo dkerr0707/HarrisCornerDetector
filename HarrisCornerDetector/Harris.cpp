d5c5eb847f6d5247c860e67cd6060fa0985ebf37
dkerr0707 <dkerr0707@gmail.com>
2019-02-06 10:41:17 -0500

    Working Gradients

--- HarrisCornerDetector/Harris.cpp
+++ HarrisCornerDetector/Harris.cpp
@@ -11,19 +11,41 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 
+#include <iostream>
+
 void Harris::Run() {
     
-    const std::string windowName = "Gray";
-    namedWindow(windowName, cv::WINDOW_AUTOSIZE );
-    imshow(windowName, GetGray() );
-    
-    cv::Mat blurred;
     cv::Size kernelSize(5, 5);
+    cv::Mat blurred;
     
     cv::GaussianBlur( GetGray(), blurred, kernelSize, 0, 0 );
     
-    const std::string windowName2 = "Blurred";
-    namedWindow(windowName2, cv::WINDOW_AUTOSIZE );
-    imshow(windowName2, blurred );
+    int scale = 1;
+    int delta = 0;
+    int depth = CV_16S;
+    cv::Mat gradientX;
+    cv::Mat gradientXAbs;
     
+    cv::Sobel( GetGray(), gradientX, depth, 1, 0, 3, scale, delta, cv::BORDER_DEFAULT );
+    convertScaleAbs( gradientX, gradientXAbs );
+    
+    cv::Mat gradientY;
+    cv::Mat gradientYAbs;
+    cv::Sobel( GetGray(), gradientY, depth, 0, 1, 3, scale, delta, cv::BORDER_DEFAULT );
+    convertScaleAbs( gradientY, gradientYAbs );
+    
+    cv::Size s = gradientYAbs.size();
+    std::cout << s.width << " " << s.height << std::endl;
+    
+    
+    
+    
+    
+    const std::string windowName2 = "Gradient X";
+    namedWindow(windowName2, cv::WINDOW_AUTOSIZE );
+    imshow(windowName2, gradientXAbs );
+    
+    const std::string windowName3 = "Gradient Y";
+    namedWindow(windowName3, cv::WINDOW_AUTOSIZE );
+    imshow(windowName3, gradientYAbs );
 }
