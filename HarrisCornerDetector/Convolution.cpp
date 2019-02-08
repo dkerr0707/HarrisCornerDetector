8a67639d545c0ede431e755673709392260b5603
dkerr0707 <dkerr0707@gmail.com>
2019-02-08 12:58:18 -0500

    Removed cpp file

--- HarrisCornerDetector/Convolution.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  Convolution.cpp
-//  HarrisCornerDetector
-//
-//  Created by David Kerr on 2019-02-08.
-//  Copyright © 2019 David Kerr. All rights reserved.
-//
-
-#include "Convolution.hpp"
